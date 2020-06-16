08b18a42462a4a68605babe05a027ae585856faa
Nirmal Kumar <54719308+NirmalKnock@users.noreply.github.com>
2020-06-16 20:34:31 +0530

    Create PWM led control.py

---
+++ LED blink/PWM led control.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(8,GPIO.OUT)
+while True:
+	pwm_obj=GPIO.PWM(8,400)
+	pwm_obj.start(100) 
+	time.sleep(1)
+	pwm_obj.ChangeDutyCycle(20)
+	time.sleep(3)
+	pwm_obj.ChangeDutyCycle(5)
+	time.sleep(3)
+	pwm_obj.ChangeDutyCycle(30)
+	time.sleep(3)
+
