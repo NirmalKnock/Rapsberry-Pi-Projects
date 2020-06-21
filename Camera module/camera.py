05f05b19b94015e7049c213aed352128c0df723c
Nirmal Kumar <54719308+NirmalKnock@users.noreply.github.com>
2020-06-21 15:22:46 +0530

    Update camera.py

--- Camera module/camera.py
+++ Camera module/camera.py
@@ -1,17 +1,2 @@
-import socket
-import sys
-mysock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-try:
-    mysock.bind("",80)
-except socket.error:
-    print("Failed to connect")
-    sys.exit()
-mysock.listen(5)
-while True:
-    conn,addr=mysock.accept()
-    data=conn.recv(1000)
-    if not data:
-        break
-    conn.sendall(data)
-conn.close()
-mysock.close()
+import picamera
+camera=picamera.PiCamera() #creating a object
